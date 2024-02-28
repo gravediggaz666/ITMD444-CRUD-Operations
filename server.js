0e41539fbba0cd3198377ff135d78d35699cf3fe
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2024-02-27 22:02:41 -0600

    containes title, descriotio

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to Dinora's Application!" });
 });
 
 require("./app/routes/tutorial.routes.js")(app);
